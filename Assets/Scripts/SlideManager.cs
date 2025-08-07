42282ffab6707460229121a37ae423596ec9f496
NaritaShosei <onigirikun7171@gmail.com>
2025-08-07 18:34:15 +0900

    [add]ドアのマネージャー

---
+++ Assets/Scripts/SlideManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SlideManager : MonoBehaviour
+{
+    [SerializeField] private SlideDoor[] _doors;
+
+    public void Flick(SlideType type)
+    {
+        Debug.Log(type.ToString());
+    }
+}
