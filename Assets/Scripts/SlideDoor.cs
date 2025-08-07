00279f234d68bb2cf8521a92ec0ec7f7076f5595
NaritaShosei <onigirikun7171@gmail.com>
2025-08-07 18:34:03 +0900

    [add]スライド

---
+++ Assets/Scripts/SlideDoor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class SlideDoor
+{
+    [SerializeField] private SlideType _slideType;
+
+    public bool CanSlide(SlideType type)
+    {
+        return _slideType == type;
+    }
+}
+public enum SlideType
+{
+    Right,
+    Left,
+    Up,
+    Down,
+}
