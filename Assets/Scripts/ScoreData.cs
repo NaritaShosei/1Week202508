089916629b828af31f00f314c8d3b6734d0fa0d7
NaritaShosei <onigirikun7171@gmail.com>
2025-08-08 19:29:15 +0900

    [add]データ

---
+++ Assets/Scripts/ScoreData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScoreData", menuName = "ScoreData")]
+public class ScoreData : ScriptableObject
+{
+    public List<Data> Datas = new List<Data>();
+}
+
+[System.Serializable]
+public class Data
+{
+    public int Score;
+    public string Name;
+}
