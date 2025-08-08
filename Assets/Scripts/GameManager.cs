92e136854d0fa23548de198f14556cf6305e2005
NaritaShosei <onigirikun7171@gmail.com>
2025-08-08 19:29:03 +0900

    [update]データの取得

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,5 +2,14 @@
 
 public class GameManager : MonoBehaviour
 {
-    
+    ScoreData _data;
+    private void Start()
+    {
+        _data = (ScoreData)Resources.Load("ScoreData");
+    }
+
+    public void SetScore(Data data)
+    {
+        _data.Datas.Add(data);
+    }
 }
