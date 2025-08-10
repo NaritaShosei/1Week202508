60f3bfd9875ee1c88a141ab064493af24131d81d
NaritaShosei <onigirikun7171@gmail.com>
2025-08-10 16:52:21 +0900

    [add]FadeUI

---
+++ Assets/Scripts/FadeUI.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeUI : MonoBehaviour
+{
+    [SerializeField] private float _duration = 0.5f;
+    [SerializeField] private Image _image;
+
+    public void Fade(float target, Action onComplete = null)
+    {
+        _image.DOFade(target, _duration).SetLink(gameObject).OnComplete(() => onComplete?.Invoke());
+    }
+}
