86d3b7b4907efbc08a77a28344c47d37d5243319
NaritaShosei <onigirikun7171@gmail.com>
2025-08-10 15:57:04 +0900

    [add]タイマーのUI

---
+++ Assets/Scripts/TimerView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerView : MonoBehaviour
+{
+    [SerializeField] private Image _view;
+
+    public void ViewUpdate(float amount)
+    {
+        _view.fillAmount = amount;
+    }
+}
