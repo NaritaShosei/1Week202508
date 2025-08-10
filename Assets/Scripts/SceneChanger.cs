e4243c19469d51ec349a649e0cea5f84c1048736
NaritaShosei <onigirikun7171@gmail.com>
2025-08-10 14:26:54 +0900

    [add]シーン遷移クラス

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    /// <summary>
+    /// 名前でシーン遷移
+    /// </summary>
+    /// <param name="name"></param>
+    public static void SceneChange(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+    /// <summary>
+    /// シーンの番号でシーン遷移
+    /// </summary>
+    /// <param name="num"></param>
+    public static void SceneChange(int num)
+    {
+        SceneManager.LoadScene(num);
+    }
+}
