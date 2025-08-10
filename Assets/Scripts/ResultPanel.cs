ae5f308b56cbad6c47a07ad9438de2f44009f329
NaritaShosei <onigirikun7171@gmail.com>
2025-08-10 19:09:32 +0900

    [add]リザルト表示

---
+++ Assets/Scripts/ResultPanel.cs
@@ -0,0 +1,13 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class ResultPanel : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _group;
+    [SerializeField] private float _duration = 0.5f;
+    public void FadePanel()
+    {
+        _group.blocksRaycasts = true;
+        _group.DOFade(1, _duration).SetLink(gameObject);
+    }
+}
