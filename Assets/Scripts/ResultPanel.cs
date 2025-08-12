c1783e65801cc097e6994fa3354cd505663a0781
NaritaShosei <onigirikun7171@gmail.com>
2025-08-12 21:09:16 +0900

    [fix]修正

--- Assets/Scripts/ResultPanel.cs
+++ Assets/Scripts/ResultPanel.cs
@@ -5,6 +5,16 @@ public class ResultPanel : MonoBehaviour
 {
     [SerializeField] private CanvasGroup _group;
     [SerializeField] private float _duration = 0.5f;
+
+    private void Awake()
+    {
+        if (_group != null)
+        {
+            _group = GetComponent<CanvasGroup>();
+        }
+        _group.blocksRaycasts = false;
+    }
+
     public void FadePanel()
     {
         if (_group == null) return;
